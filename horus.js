2e0215afed83b872df2a6ed08ec1acb60bad9607
Alexander James Young <ayoungj@umich.edu>
2020-07-21 17:09:29 -0400

    need to work on server now

--- horus.js
+++ horus.js
@@ -1,18 +1,32 @@
 
 class horus {
-  constructor() {
+  constructor(name) {
+    this.serviceName = name;
     this.startTime = 0;
     this.endTime = 0;
     this.traceTime = 0;
+    this.route = {}; // every element in route represents a request. each request can have requests nested in it
   }
-  start() {
-    this.startTime = Number(process.hrtime.bigint());    
+
+  //{serviceName : {}, responsesTime: null}
+  //{serviceName: {books: {}, responseTime: null}, responsesTime: 5ms}
+  start(targetService) {
+    this.startTime = Number(process.hrtime.bigint());
+    this.route[targetService] = {};
+    this.route.responseTime = null;
+    console.log('logging object in route ', this.route)
+    // { { targetService : {}, responseTime: null }  }
   }
+
   end() {
     this.endTime = Number(process.hrtime.bigint());
     this.traceTime = this.endTime - this.startTime;
-    console.log('startTime was ', this.startTime,  ' endTime ', endTime);
+    this.route.responseTime = this.traceTime;
+    console.log('startTime was ', this.startTime,  ' endTime ', this.endTime);
     console.log('Difference was: ', this.traceTime);
+
+    console.log('logging changed route ', this.route)
+
   }
 }
 
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const booksStub = require('./stubs/booksStub.js');
 const customersStub = require('./stubs/customersStub.js');
 const grpc = require("grpc");
+const horusTracer = require('./horus.js');
+
 
 const book = {
   title: 'ITttttt', 
@@ -9,6 +11,8 @@ const book = {
   publisher: 'Random House',
   id: 200
 };
+
+let ht = new horusTracer('main');
 
 //Make a request from main initiator to books using booksStub.
 // const bStub = booksStub;
@@ -20,7 +24,9 @@ meta.add('la', 'lala');
 // );
 
 function main() {
+    ht.start('books');
     booksStub.CreateBook(book, meta, (error, response) => {
+        ht.end();
         console.log(response);
     }).on('metadata', metadata => {
         console.log('now logging metadata ', metadata._internal_repr.la);
